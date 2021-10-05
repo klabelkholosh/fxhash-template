ddedb1ef169851eb75c7b447bd1497299f6549e7
Carl Schmidt <klabelkholosh@gmail.com>
2021-10-05 15:20:18 +0200

    further pairing back, random date anim add

--- src/art.js
+++ src/art.js
@@ -35,6 +35,7 @@ let divNum;
 let rotCh;
 let curveCh;
 let animCounter = 0;
+let randomDOY;
 
 //-----------------------------------------------------------------------------
 // functions
@@ -45,11 +46,11 @@ let animCounter = 0;
  */
 function setup() {
 
-  // initial state
-  resetState();
+  // in case tokenState not set, initialize it
+  initialTokenState();
 
   // grab hash from tokenData and use it to create a p5js randomSeed
-  // any 'random' call after this is always deterministic to the supplied seed!
+  // any random() call after this is always deterministic to the supplied seed!
   console.log('tokenData.hash is: ', tokenData.hash);
   hash = tokenData.hash;
   seed = parseInt(hash.slice(0, 16), 16)
@@ -64,6 +65,9 @@ function setup() {
   pStartY       = canvasHeight * 0.25;    
   pEndY         = canvasHeight * 0.75;      
   lineLayers    = canvasHeight / 2;
+
+   // default frame-rate is 8
+  frameRate(8);                                              
 
   // all random variables!
   rdPal = random(pallettes);                                                          // pick a random color
@@ -80,7 +84,8 @@ function setup() {
   }
   stStrWght = parseInt(random(2,4));                                                  // the line weight of each strand can be between 2 (stringy) - 4 (solid)
   curveCh = parseInt(random(1, 10)) >= 9 ? true : false;                              // 2 in 10 chance of sweeping bezier curves for some divisions
-
+  randomDOY = randomDate(new Date(2012, 0, 1), new Date());                           // on a random day/month of the year, the script will slide-animate for the entire day.
+  
   // create canvas
   canvas = createCanvas(canvasWidth, canvasHeight);
 
@@ -112,6 +117,7 @@ function setup() {
   metadata.strokeWeight = stStrWght;
   metadata.rotation = rotCh ? 'ascending' : 'regular';
   metadata.lineStyle = curveCh ? 'curved' : 'straight';
+  metadata.lifting = randomDOY.getDate() + '-' + randomDOY.getMonth();
   console.log(metadata);
 
 }
@@ -123,42 +129,23 @@ function setup() {
  *  global scope.
  */
 function draw() {
-
-  if(tokenState.animateThis) {
-    animCounter = animCounter + 100;
-  }
-  /*
-  const {
-    shape,
-    color
-  } = hashToTraits(tokenData.hash);
-
-  const hexToRGB = color => Array.from(Array(3).keys())
-    .map(i => i * 2 + 1)
-    .map(i => fromHex(color.slice(i, i + 2)));
-
-  // step
-  tokenState.tf = (tokenState.tf + 0.05) % (Math.PI * 2);
-  */
+  
   background(bgCl);
-
-  frameRate(8 + tokenState.frameRate);                        // default frame-rate is 8, plus/minus user setting
   fltTwk = 10 + tokenState.flutterAdd;                        // default flutter tweak is 10, plus/minus user setting
-
   noFill();
-
-  // animation attempts
-  if (tokenState.animateThis === true) {
-    tokenState.divXSlide = sin(animCounter / tokenState.animSpeed) * 100;
-    tokenState.flattenAng = sin(animCounter / tokenState.animSpeed) * 100;
-  }
-
 
   // for as many portals as are defined (always 1, currently), draw it!
   for(let p = 0; p < portals.length; p++) {
     drawPortal(
         ...portals[p]
      );
+  }
+
+  // if a particular day and month, slowly animate the script further for the entire day.
+  if ((new Date().getDate() === randomDOY.getDate()) && (new Date().getMonth() === randomDOY.getMonth())) {
+    animCounter = animCounter + 100;
+    tokenState.divXSlide = sin(animCounter / 8000) * 100;
+    tokenState.flattenAng = sin(animCounter / 8000) * 100;
   }
 }
 
@@ -168,28 +155,14 @@ function keyPressed() {
     paused = !paused;
     paused ? noLoop() : loop();
   }
-
-  // reset the state
-  if ( key == 'r' ) {
-    resetState();
-  }
-
-  // animate / unanimate it
-  if ( key == 'a' ) {
-    tokenState.animateThis = !tokenState.animateThis;
-  }
 }
 
-// set user settings back to original
-function resetState() {
-  tokenState.divXSlide = 0;
-  tokenState.lineThickness = 0;
-  tokenState.flattenAng = 0;
-  tokenState.frameRate = 0;
-  tokenState.flutterAdd = 0;
-  tokenState.animateThis = false;
-  tokenState.animSpeed = 8000;
-  animCounter = 0;
+// in case tokenState not set, initialize it
+function initialTokenState() {
+  if (typeof tokenState.lineThickness === 'undefined') { tokenState.lineThickness = 0; }
+  if (typeof tokenState.flutterAdd === 'undefined') { tokenState.flutterAdd = 0; }
+  if (typeof tokenState.divXSlide === 'undefined') { tokenState.divXSlide = 0; }
+  if (typeof tokenState.flattenAng === 'undefined') { tokenState.flattenAng = 0; }
 }
 
 //-----------------------------------------------------------------------------------------------------------------------------------
@@ -208,13 +181,13 @@ function drawPortal(heightPos, heightPosIncr, xStart, xEnd, yStart, yEnd, sentDi
   for (let i = 0; i < lineLayers; i++) {
 
     // set a stroke weight
-    strokeWeight(stStrWght + tokenState.lineThickness); // old was random(1)
+    strokeWeight(stStrWght + tokenState.lineThickness);
 
-    // define slightly random widths / line height starting positions - they're mostly straight, but need to jiggle and look like interference / static...
+    // define slightly random widths / line height starting positions, to create flutter effect
     let xpos1 = xStart + random(fltTwk);
     let xpos2 = xEnd - random(fltTwk);
-    let ypos1 = yStart + heightPos + random(fltTwk / 2); // heightPos + 90
-    let ypos2 = yStart + heightPos + random(fltTwk / 2); // heightPos - 45;
+    let ypos1 = yStart + heightPos + random(fltTwk / 2);
+    let ypos2 = yStart + heightPos + random(fltTwk / 2);
 
     // draw an interference line
     if (ypos1 < yEnd) { // this 'if' is what cuts it off at the bottom. maybe needs work... TODO
@@ -249,11 +222,9 @@ function drawPortal(heightPos, heightPosIncr, xStart, xEnd, yStart, yEnd, sentDi
         drawLine(tempXPos2, xpos2, ypos1, ypos2, 0, 0, false);
           
       } else {
-        // curveCh
         drawLine(xpos1, xpos2, ypos1, ypos2, slantAdd + tokenState.flattenAng, 0, curveCh, xStart, xEnd, yStart, yEnd, "uncut");
 
         // shadow line, if we're slanted
-        // curveCh
         if (slantAdd + tokenState.flattenAng > 0) {
           drawLine(xpos1, xpos2, ypos1, ypos2, 0, 0, false, xStart, xEnd, yStart, yEnd, "shadow_light");
         }
@@ -276,79 +247,79 @@ function drawPortal(heightPos, heightPosIncr, xStart, xEnd, yStart, yEnd, sentDi
 // when we want to draw line that doesn't have a vertical division.. basically a 'solid' line. can be flat, or 'lifted'. and can curve crazily.
 function drawLine(xpos1, xpos2, ypos1, ypos2, slantAdd, ySlantTweak, curveCh, xStart, xEnd, yStart, yEnd, lineStyle, counter, dvs) {
 
-let 
-  cpx1,
-  cpy1,
-  cpx2,
-  cpy2,
-  currCutX;
+  let 
+    cpx1,
+    cpy1,
+    cpx2,
+    cpy2,
+    currCutX;
 
-if (lineStyle === 'shadow_dark') {
-  stroke(changeColourPercentage(random(rdPal), random(0.1, 0.4)));
-} else if (lineStyle === 'shadow_light') {
-  stroke(changeColourPercentage(random(rdPal), random(0.5, 0.75)));
-} else if (lineStyle === 'droopy') {
-  currCutX = dvs.find((el) => counter < el.cutY).cutX;
-  // bezier curve points 
-  cpx1 = (random(xpos1 + ((xEnd - xStart) / 20), xpos1)) + ((xEnd - xStart) / 4), 
-  cpy1 = (random(ypos1 + ((yEnd - yStart) / 20), ypos1) + random(fltTwk / 2)), 
-  cpx2 = (random(currCutX + ((xEnd - xStart) / 20), currCutX) + random(fltTwk)),  
-  cpy2 = (random(ypos2 + ((yEnd - yStart) / 20), ypos2)) - ((yEnd - yStart) / 20);
-} else if (lineStyle === 'pouring') {
-  currCutX = dvs.find((el) => counter < el.cutY).cutX;
-  // bezier curve points
-  cpx1 = (random(xpos1 + ((xEnd - xStart) / 20), xpos1)) + ((xEnd - xStart) / 4),
-  cpy1 = (random(ypos1 + ((yEnd - yStart) / 20), ypos1) - ySlantTweak + random(fltTwk / 2)) + ((yEnd - yStart) / 20),
-  cpx2 = (random(currCutX + ((xEnd - xStart) / 20), currCutX) + random(fltTwk)) - ((xEnd - xStart) / 40),
-  cpy2 = (random(ypos2 + ((yEnd - yStart) / 20), ypos2)) - ((yEnd - yStart) / 20);
-  // console.log('cpy1:', cpy1);
-}
-
-if (lineStyle === 'droopy' || lineStyle === 'pouring') {
-      // ensure we don't go over the right-hand width
-      tempLen = currCutX + random(fltTwk) + ((xEnd - xStart) / 20) + tokenState.divXSlide;
-      tempXPos2 =  tempLen > xpos2 ? xpos2 :  tempLen < xpos1 ? xpos1 : tempLen;
-  
-      bezier(
-          xpos1, 
-          ypos1 - ySlantTweak + random(fltTwk / 2), 
-          cpx1, 
-          cpy1, 
-          cpx2, 
-          cpy2, 
-          tempXPos2, 
-          ypos2
-      );
-} else {
-  let tempYPos2 = slantAdd > 0 ? ypos2 - slantAdd + random(fltTwk / 2) : ypos2;
-  if (curveCh) {
+  if (lineStyle === 'shadow_dark') {
+    stroke(changeColourPercentage(random(rdPal), random(0.1, 0.4)));
+  } else if (lineStyle === 'shadow_light') {
+    stroke(changeColourPercentage(random(rdPal), random(0.5, 0.75)));
+  } else if (lineStyle === 'droopy') {
+    currCutX = dvs.find((el) => counter < el.cutY).cutX;
+    // bezier curve points 
+    cpx1 = (random(xpos1 + ((xEnd - xStart) / 20), xpos1)) + ((xEnd - xStart) / 4), 
+    cpy1 = (random(ypos1 + ((yEnd - yStart) / 20), ypos1) + random(fltTwk / 2)), 
+    cpx2 = (random(currCutX + ((xEnd - xStart) / 20), currCutX) + random(fltTwk)),  
+    cpy2 = (random(ypos2 + ((yEnd - yStart) / 20), ypos2)) - ((yEnd - yStart) / 20);
+  } else if (lineStyle === 'pouring') {
+    currCutX = dvs.find((el) => counter < el.cutY).cutX;
     // bezier curve points
-    let 
-    cpx1 = (xStart + xEnd) / 2, 
-    cpy1 = (ypos1 - ((yEnd - yStart) / 4)), 
-    cpx2 = (xStart + xEnd) / 2,  
-    cpy2 = (ypos1 + ((yEnd - yStart) / 4));
-     
-    bezier(
-      xpos1, 
-      ypos1 - ySlantTweak + random(fltTwk / 2), 
-      cpx1, 
-      cpy1, 
-      cpx2, 
-      cpy2, 
-      xpos2, 
-      tempYPos2
-    );
-  
-  } else {
-    line(
-      xpos1, 
-      ypos1, 
-      xpos2, 
-      tempYPos2
-    );
+    cpx1 = (random(xpos1 + ((xEnd - xStart) / 20), xpos1)) + ((xEnd - xStart) / 4),
+    cpy1 = (random(ypos1 + ((yEnd - yStart) / 20), ypos1) - ySlantTweak + random(fltTwk / 2)) + ((yEnd - yStart) / 20),
+    cpx2 = (random(currCutX + ((xEnd - xStart) / 20), currCutX) + random(fltTwk)) - ((xEnd - xStart) / 40),
+    cpy2 = (random(ypos2 + ((yEnd - yStart) / 20), ypos2)) - ((yEnd - yStart) / 20);
+    // console.log('cpy1:', cpy1);
   }
-}
+
+  if (lineStyle === 'droopy' || lineStyle === 'pouring') {
+        // ensure we don't go over the right-hand width
+        tempLen = currCutX + random(fltTwk) + ((xEnd - xStart) / 20) + tokenState.divXSlide;
+        tempXPos2 =  tempLen > xpos2 ? xpos2 :  tempLen < xpos1 ? xpos1 : tempLen;
+    
+        bezier(
+            xpos1, 
+            ypos1 - ySlantTweak + random(fltTwk / 2), 
+            cpx1, 
+            cpy1, 
+            cpx2, 
+            cpy2, 
+            tempXPos2, 
+            ypos2
+        );
+  } else {
+    let tempYPos2 = slantAdd > 0 ? ypos2 - slantAdd + random(fltTwk / 2) : ypos2;
+    if (curveCh) {
+      // bezier curve points
+      let 
+      cpx1 = (xStart + xEnd) / 2, 
+      cpy1 = (ypos1 - ((yEnd - yStart) / 4)), 
+      cpx2 = (xStart + xEnd) / 2,  
+      cpy2 = (ypos1 + ((yEnd - yStart) / 4));
+      
+      bezier(
+        xpos1, 
+        ypos1 - ySlantTweak + random(fltTwk / 2), 
+        cpx1, 
+        cpy1, 
+        cpx2, 
+        cpy2, 
+        xpos2, 
+        tempYPos2
+      );
+    
+    } else {
+      line(
+        xpos1, 
+        ypos1, 
+        xpos2, 
+        tempYPos2
+      );
+    }
+  }
 }
 
 // when we want to draw one of the 'cutX' thread lines..
@@ -365,66 +336,67 @@ function drawCutXLine(dvs, xpos1, ypos1, ypos2, counter, ySlantTweak, endXpos) {
     ypos2
   );
 }
+
 //-----------------------------------------------------------------------------------------------------------------------------------
 // DIVISION-SPECIFIC FUNCTIONS
 //-----------------------------------------------------------------------------------------------------------------------------------
 
 function createDvs(rdPal, lineLayers, pStartX, pEndX, divNum) {
-let dvs = [];
-let runningCut = parseInt(lineLayers / divNum);
-let pi = 0;
-let cutXRandomChance = 0;
-let tempColor = '#000000';
+  let dvs = [];
+  let runningCut = parseInt(lineLayers / divNum);
+  let pi = 0;
+  let cutXRandomChance = 0;
+  let tempColor = '#000000';
 
-for (let i = 0; i < divNum; i++) {
-  cutXRandomChance = random(1, 10);
-  
-  // ensure we cycle through pallette colours, rather than running out if undefined
-  if (typeof rdPal[`${i + 1}`] === 'undefined'){
-    tempColor = rdPal[`${pi}`];
-    pi++;
-  } else {
-    tempColor = rdPal[`${i + 1}`];
-  }
-
-  // determine whether we randomly add a cutX
-  if (cutXRandomChance > 4) {
-    cutX = random((pStartX + fltTwk), (pEndX - fltTwk));
-    colorX = random(rdPal);
-    if(random(10) <= 8){
-        cutXstyle = null; //none
+  for (let i = 0; i < divNum; i++) {
+    cutXRandomChance = random(1, 10);
+    
+    // ensure we cycle through pallette colours, rather than running out if undefined
+    if (typeof rdPal[`${i + 1}`] === 'undefined'){
+      tempColor = rdPal[`${pi}`];
+      pi++;
     } else {
-      if(random(10) <= 5){
-        cutXstyle = "droopy"; //droopy
-      } else {
-        cutXstyle = "pouring"; //pouring
-      }
+      tempColor = rdPal[`${i + 1}`];
     }
-  } else {
-    cutX = null;
-    colorX = null;
-    cutXstyle = null;
+
+    // determine whether we randomly add a cutX
+    if (cutXRandomChance > 4) {
+      cutX = random((pStartX + fltTwk), (pEndX - fltTwk));
+      colorX = random(rdPal);
+      if(random(10) <= 8){
+          cutXstyle = null; //none
+      } else {
+        if(random(10) <= 5){
+          cutXstyle = "droopy"; //droopy
+        } else {
+          cutXstyle = "pouring"; //pouring
+        }
+      }
+    } else {
+      cutX = null;
+      colorX = null;
+      cutXstyle = null;
+    }
+    
+    let tempObj = {
+      cutY: runningCut,
+      color: tempColor,
+      cutX: cutX,
+      colorX: colorX,
+      cutXstyle: cutXstyle
+    };
+
+    // push to end of array
+    dvs.push(tempObj);
+
+    // update string .. if 1 before end, replace with just total number of line layers
+    runningCut = runningCut + parseInt(lineLayers / divNum);
   }
-  
-  let tempObj = {
-    cutY: runningCut,
-    color: tempColor,
-    cutX: cutX,
-    colorX: colorX,
-    cutXstyle: cutXstyle
-  };
+    
+  // make sure last element is set to total lineLayers to fix occasional bug
+  dvs[divNum - 1].cutY = lineLayers;
 
-  // push to end of array
-  dvs.push(tempObj);
-
-  // update string .. if 1 before end, replace with just total number of line layers
-  runningCut = runningCut + parseInt(lineLayers / divNum);
-}
-  
-// make sure last element is set to total lineLayers to fix occasional bug
-dvs[divNum - 1].cutY = lineLayers;
-
-return dvs;
+  return dvs;
 }
 
 function setColorY(i, dvs) {
@@ -454,6 +426,10 @@ function checkXDiv(i, dvs) {
   return false;
 }
 
+//-----------------------------------------------------------------------------------------------------------------------------------
+// OTHER FUNCTIONS
+//-----------------------------------------------------------------------------------------------------------------------------------
+
 // dulls / intensifies a colour by changing its percentage. Wow!
 function changeColourPercentage(colour, percentage) {
   if(typeof colour !== 'undefined'){
@@ -468,17 +444,14 @@ function changeColourPercentage(colour, percentage) {
   } 
 }
 
-//-----------------------------------------------------------------------------------------------------------------------------------
-// ARTBLOCKS FUNCTIONS
-//-----------------------------------------------------------------------------------------------------------------------------------
-
-function random_hash() {
-  let x = "0123456789abcdef", hash = '0x'
-  for (let i = 64; i > 0; --i) {
-    hash += x[Math.floor(Math.random()*x.length)]
-  }
-  return hash
+// on setup, get a random date (deterministic from seed)
+function randomDate(start, end) {
+  return new Date(start.getTime() + random(1) * (end.getTime() - start.getTime()));
 }
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+// PALLETTES - STORED HERE, OUT OF THE WAY..
+//-----------------------------------------------------------------------------------------------------------------------------------
 
 let pallettes = [
   ["#d9ceb2","#948c75","#d5ded9","#7a6a53","#99b2b7"],
