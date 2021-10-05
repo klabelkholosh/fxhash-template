2ccbe3605bcab4b39a9b50d7d7b20b9f7ba7f93e
Carl Schmidt <klabelkholosh@gmail.com>
2021-10-05 13:38:16 +0200

    pairing back

--- app/index.js
+++ app/index.js
@@ -6,26 +6,10 @@
 /**
  * Handle speed state change.
  */
-const onChangeDivXSlide = value => {
-  tokenState.divXSlide = parseInt(value);
-};
-
-const onChangeFlattenAng = value => {
-  tokenState.flattenAng = parseInt(value);
-};
-
-const onChangeFrameRate = value => {
-  tokenState.frameRate = parseInt(value);
-};
-
 const onChangeFlutterAdd = value => {
   tokenState.flutterAdd = parseInt(value);
 };
 
 const onChangeLineThickness = value => {
   tokenState.lineThickness = parseInt(value);
-};
-
-const onChangeAnimSpeed = value => {
-  tokenState.animSpeed = parseInt(value);
 };
